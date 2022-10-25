8d52b0ec9be22c03145a750a346d8a665ea7dbf1
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-10-25 20:03:56 +0530

    Create PracticeLoops.java

---
+++ PracticeLoops.java
@@ -0,0 +1,16 @@
+public class PracticeLoops {
+    
+    public static void practiceDoWhileLoop(){
+
+        int x=1;
+        do{
+            System.out.println(x);
+            x++;
+        }while(x<6);
+
+    }
+    public static void main(String[] args) {
+        practiceDoWhileLoop();   
+    }
+
+}
