134b2ee75e01b20052dd368bf2467540984b3c30
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-11-01 23:08:19 +0530

    Create Q-2.java

---
+++ Assignment/Q-2.java
@@ -0,0 +1,8 @@
+----------------------------------------------------------------------------------
+
+How many visible characters does the following println statement produce when run?
+  
+System.out.println("\t\nn\\\t\"\tt");
+
+----------------------------------------------------------------------------------
+Ans - 4
