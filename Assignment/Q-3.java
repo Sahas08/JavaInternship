5c78c9e13b2a432a72b733ddb3d337d9dc8612d2
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-11-01 23:12:55 +0530

    Create Q-3.java

---
+++ Assignment/Q-3.java
@@ -0,0 +1,25 @@
+What sequence of println statements will generate the following output?
+  
+----------------------------------------------------
+This program prints the first lines
+of the song "slots".
+"She lives in a trailer"
+"On the outskirts 'a Reno"
+"She plays quarter slots in the local’s casino."
+----------------------------------------------------
+  
+ package InternshipAssignment;
+
+public class Q_3 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("This program prints the first lines of the song \"slots\"");
+        System.out.println();
+        System.out.println("\"She lives in a trailer\"");
+        System.out.println("\"On the outskirts \'a Reno\"");
+        System.out.println("\"She plays quarter slots in the local's casino.");
+
+    }
+
+}
