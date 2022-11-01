340f01a72526f966c23da262f443c1cd02454438
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-11-01 23:15:43 +0530

    Create Q-4.java

---
+++ Assignment/Q-4.java
@@ -0,0 +1,29 @@
+What sequence of println statements will generate the following output?
+  
+---------------------------------------
+A "quoted" String is
+'much' better if you learn
+the rules of "escape sequences."
+Also, "" represents an empty String.
+Don't forget to use \" instead of " !
+'' is not the same as "
+---------------------------------------
+  
+ 
+package InternshipAssignment;
+
+public class Q_4 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("A \"quoted\" String is");
+        System.out.println("\'much\' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println();
+        System.out.println("Also, \"\" represents an empty String.");
+        System.out.println("Don\'t forget to use \\\" instead of \" !");
+        System.out.println("\'\' is not the same as \"");
+
+    }
+
+}
