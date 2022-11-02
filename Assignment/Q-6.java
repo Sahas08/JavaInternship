36cd9ae59a9f30597929e21f4a789a65f187003e
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-11-02 12:42:29 +0530

    Create Q-6.java

---
+++ Assignment/Q-6.java
@@ -0,0 +1,19 @@
+-------------------------------------------------
+
+Write a println statement to produce this output:
+/ \ // \\ /// \\\
+  
+-------------------------------------------------
+
+package InternshipAssignment;
+
+public class Q_6 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("/ \\ // \\\\ /// \\\\\\");
+
+    }
+
+}
+
