4fdb2a14751e5708b57dff4d050522c67ae1d0c4
Sahas <43925194+Sahas08@users.noreply.github.com>
2022-11-02 12:41:30 +0530

    Create Q-5.java

---
+++ Assignment/Q-5.java
@@ -0,0 +1,18 @@
+-------------------------------------------------------
+
+What is the output of the following println statements?
+System.out.println("\ta\tb\tc");
+System.out.println("\\\\");
+System.out.println("'");
+System.out.println("\"\"\"");
+System.out.println("C:\nin\the downward spiral");
+
+-------------------------------------------------------
+  
+  
+         a       b       c
+\\
+'
+"""
+C:
+in      he downward spiral
